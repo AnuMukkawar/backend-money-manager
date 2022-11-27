4db060be5b22359808c55c8e537a4aaa151f7112
Anumukkawar <anujamukkawar21@gmail.com>
2022-11-27 17:15:09 +0530

    new

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import {MongoClient} from 'mongodb';
+import dotenv from 'dotenv'
+import { getAllTransactions } from "./helper.js";
+import { transactionRouter} from './routes/transactions.js'
+import cors from 'cors';
+
+dotenv.config()
+
+const app=express();
+app.use(cors());
+
+const port=process.env.PORT;
+const MONGO_URL=process.env.MONGO_URL;
+// const MONGO_URL="mongodb://localhost";
+
+async function createConnection(){
+    const client=new MongoClient(MONGO_URL);
+    await client.connect();
+    return client;
+}
+
+export const client =await createConnection();
+// const expenses= await client.db("money_manager").collection("expenses").findOne({id:1});
+app.use(express.json())
+// app.get("/",(req,res)=>{
+//     res.send("Hello");
+// });
+
+app.use("/",transactionRouter);
+app.listen(port,()=>console.log("Server is started ",port));
